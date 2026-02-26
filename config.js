9784a946c2d25aae8ba30cc87ccadeafa66d2421
DominicDev <s0l0estudi4r@gmail.com>
2026-02-26 13:32:11 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PINK FLOYD';
+export const nameSongs = [
+	"TIME", /*1*/
+	"ANOTHER BRICK IN THE WALL", /*2*/
+	"THE GREAT GIG IN THE SKY", /*3*/
+	"MONEY ", /*4*/
+	"COMFORTABLY NUMB", /*5*/
+	"WISH YOU WERE HERE", /*6*/
+	"SHINE ON YOU CRAZY DIAMOND", /*7*/
+	"HEY YOU", /*8*/
+	"US AND THEM", /*9*/
+	"BRAIN DAMAGE", /*10*/
+	"ECHOES", /*11*/
+	"HIGH HOPES", /*12*/
+	"MOTHER ", /*13*/
+	"WELCOME TO THE MACHINE", /*14*/
+	"RUN LIKE HELL", /*15*/
+	"LEARNING TO FLY", /*16*/
+	"PIGS", /*17*/
+	"ONE OF THESE DAYS", /*18*/
+	"HAVE A CIGAR", /*19*/
+	"YOUNG LUST", /*20*/
+]
